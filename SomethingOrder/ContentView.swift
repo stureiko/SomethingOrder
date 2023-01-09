3ad99e7a0a82a5f1257a610d1f71140c3ed0c389
stureiko <stureiko@gmail.com>
2023-01-09 22:26:34 +0300

    Registration screen

--- SomethingOrder/ContentView.swift
+++ SomethingOrder/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Spacer()
+            Text("Autorization")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title.bold())
+                .background(Color("blueAlpha"))
+                .cornerRadius(30)
+            
+            VStack {
+                TextField("EMail", text: $email)
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(10)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(10)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                HStack {
+                    Button {
+                        print("Login")
+                    } label: {
+                        Text("Login")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color("blueAlpha"))
+                            .font(.title3.bold())
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .padding(8)
+                            .padding(.horizontal, 12)
+                    }
+                    
+                    Button {
+                        print("Register")
+                    } label: {
+                        Text("Register")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color("blueAlpha"))
+                            .font(.title3.bold())
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .padding(8)
+                            .padding(.horizontal, 12)
+                    }
+                }
+
+            }
+            .padding()
+            .background(Color("greyAlpha"))
+            .cornerRadius(20)
+            .padding()
+            Spacer()
+            Spacer()
+            
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("bg").ignoresSafeArea())
     }
 }
 

146f6ff3dfe2c4f3d6ef8cf8493a38fc368f0ed8
stureiko <stureiko@gmail.com>
2023-01-09 21:03:41 +0300

    Initial Commit

---
+++ SomethingOrder/SomethingOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingOrderApp.swift
+//  SomethingOrder
+//
+//  Created by Игорь Стурейко on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SomethingOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
